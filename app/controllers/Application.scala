5be3612cad719fd6842a77b1406dcbfb9f56126d
Mael Thomas <myt1g11@soton.ac.uk>
2012-07-31 18:04:19 +0100

    First commit with websockets, dummy tweet served to the client

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,36 +7,70 @@ import play.api.libs.concurrent.Akka
 import play.api.libs.ws.{ WS, Response }
 import akka.dispatch.Await
 import play.api.libs.iteratee.Enumerator
+import play.api.libs.iteratee.Iteratee
+import play.api.libs.json.JsValue
+import com.codahale.jerkson.Json._
+import play.api.libs.json.Json
 
+case class Tweet(id_str: String, created_at: String, usr_name: String, text: String, concepts: List[String] )
 object Application extends Controller {
 
-  def index = Action { request =>
-    Ok("Got request [" + request + "]")
+  def index = Action { implicit request =>
+    Ok(views.html.index("Welcome :)"))
   }
 
   def articleFromId(id: Long) = {
     val url = "http://en.wikipedia.org/w/api.php?format=xml&action=query&pageids=" + id
-      val promiseOfTitle = WS.url(url).get().map { response =>
-        (response.xml \\ "page" \ "@title").toString
-      }
+    val promiseOfTitle = WS.url(url).get().map { response =>
+      (response.xml \\ "page" \ "@title").toString
+    }
     articleFromTitle(promiseOfTitle)
   }
 
   def articleFromTitle(title: Any) = Action {
     //TODO : correctly handle title formating
     val res = title match {
-      case title: String =>  WS.url("http://en.m.wikipedia.org/wiki/" + title).get()
+      case title: String => WS.url("http://en.m.wikipedia.org/wiki/" + title).get()
       case promise: Promise[String] => promise.flatMap(title => WS.url("http://en.m.wikipedia.org/wiki/" + title).get())
       // How to remove this warning ?
     }
     Async {
-     
-      res.map { n => SimpleResult(
-        header = ResponseHeader(200, Map(CONTENT_TYPE -> "text/html")),
-        body = Enumerator(n.body))
-        }
+
+      res.map { n =>
+        SimpleResult(
+          header = ResponseHeader(200, Map(CONTENT_TYPE -> "text/html")),
+          body = Enumerator(n.body))
+      }
     }
 
+  }
+
+  def inlayTweets(n: Response) = {
+    val titleNode = (n.xml \\ "h1" \ "@id") find { _.text == "firstHeading" } get
+    val title = titleNode.text
+
+    val tweets = <span>Tweets about @title will be displayed here</span>
+
+    val content = (n.xml \\ "div" \ "@id") find { _.text == "content" }
+    content.map(div => (div \\ "p").head)
+  }
+
+  def ws = WebSocket.using[JsValue] { request =>
+
+    // Just consume and ignore the input, we only need to establish a connection
+    val in = Iteratee.foreach[JsValue]{ event =>
+      
+    }
+
+    // Tweets will be sent through this channel
+    // id_str: String, created_at: String, usr_name: String, text: String, concepts: List[String] 
+    // All the information to display have to be there, we don't want to call the Twitter API client side. 
+    val t = Tweet("81979798765", "Wed Aug 27 13:08:45 +0000 2008", "laem", 
+        "La voie pour l'avenir de l'humanité", List("La Voie", "Edgar Morin"))
+    
+    val out = Enumerator(Json.parse(generate(t))) >>> Enumerator.eof
+
+    (in, out)
   }
 
 }
